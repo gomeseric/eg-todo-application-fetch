7b057b21e9ce84583934e4e20d98621ae3d47883
gomeseric <gomesericson.a@gmail.com>
2024-07-16 00:59:54 +0000

    test

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,29 @@
+"use client";
+
+import { getUsers } from "@/services/getUsers";
+import {useEffect, useState } from "react";
+
 export default function Home() {
-  return (
-    <main className="min-h-screen flex-col">
-      <div className='container mx-auto text-2xl'>Hello</div>
-    </main>
-  );
+  const [users, setUsers] = useState([]);
+
+  const loadUsers = async () => {
+    const data = await getUsers();
+    console.log(data);
+    setUsers(data);
+  };
+
+  useEffect(() => {
+    loadUsers();
+}, []);
+
+return (
+  <div>
+    List of users {users.length}
+    {users.map((user) => (
+      <div key={user.id} className="text-sm text-sky-400">
+        {user.name}
+        </div>
+    ))}
+  </div>
+);
 }
